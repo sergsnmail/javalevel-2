a368ef1747f4b95adf971a17b607808a998d3630
Sergey Pozdeev <sergsnmail@gmail.com>
2021-01-19 20:36:02 +0300

    Homework-2

---
+++ src/main/java/com/sergsnmail/lesson2/myexceptions/MyArrayDataException.java
@@ -0,0 +1,17 @@
+package com.sergsnmail.lesson2.myexceptions;
+
+public class MyArrayDataException extends Exception {
+    private String value;
+    private int i,j;
+
+    public MyArrayDataException(String value, int i, int j) {
+        this.i = i;
+        this.j = j;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Wrong data value (%s) in cell[%s][%s] of input array", value, i, j );
+    }
+}
---
+++ src/main/java/com/sergsnmail/lesson2/myexceptions/MyArraySizeException.java
@@ -0,0 +1,14 @@
+package com.sergsnmail.lesson2.myexceptions;
+
+public class MyArraySizeException extends Exception {
+    private int size;
+
+    public MyArraySizeException(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Array size must be %sx%s", size, size);
+    }
+}
